bad58d3bd858bf58f9f8bd0ac370d71e963f9f4f
qiyu1987 <qiyu1987@gmail.com>
2019-10-16 15:57:07 +0200

    add teamRouter

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const db = require('./db')
 const Team = require('./team/model')
+const teamRouter = require('./team/router')
 const app = express()
 const port = process.env.PORT || 4000
+app.use(teamRouter)
 app.listen(port, () => console.log(`Listening on Port ${port}`))
