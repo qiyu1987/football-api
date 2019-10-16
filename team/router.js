8e28d1c4ad2c082e0277ca40e95bcb5dd60f6c6c
qiyu1987 <qiyu1987@gmail.com>
2019-10-16 17:03:30 +0200

    get team on /team/:id

--- team/router.js
+++ team/router.js
@@ -13,12 +13,23 @@ router.get('/team',
 router.post('/team',
     (req, res, next) => {
         if (!req.body) {
-            res.status(404).end
+            res.status(404).end()
         }
         Team.create(req.body)
             .then(team => res.send(team))
             .catch(next) 
     }
 )
+router.get('/team/:id',
+    (req, res, next) => {
+        Team.findByPk(req.params.id)
+            .then( team => {
+                if (!team) {
+                    res.status(404).end()
+                }
+                res.send(team)
+            })
+            .catch(next)
+    })
 
 module.exports = router
