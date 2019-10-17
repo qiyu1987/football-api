305844de11ac4f376fc4fafb870e269a54b49eaf
qiyu1987 <qiyu1987@gmail.com>
2019-10-17 10:54:16 +0200

    delete db reset for heroku, keep db schema

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const databaseUrl = process.env.DATABASE_URL
     ||'postgres://postgres:secret@localhost:5432/postgres'
 const db = new Sequelize(databaseUrl)
-db.sync({force:true})
+db.sync()
     .then(() => {console.log('database schema updated')})
     .catch(console.error)
 module.exports = db
