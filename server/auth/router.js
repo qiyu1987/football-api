9ce55b2bf0d39534f7eac40ed1105858d4c75412
qiyu1987 <qiyu1987@gmail.com>
2019-10-22 12:23:34 +0200

    creat auth middleware and use in secrect-endpoint

--- server/auth/router.js
+++ server/auth/router.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const { toJWT, toData } = require('./jwt')
 const bcrypt = require('bcrypt')
+const auth = require('./middleware')
 
 const router = new Router()
 const User = require('../../user/model')
@@ -62,27 +63,30 @@ router.post('/login', (req, res, next) => {
 })
 
 
-router.get('/secret-endpoint', (req, res) => {
-    const auth = req.headers.authorization && req.headers.authorization.split(' ')
-    if (auth && auth[0] === 'Bearer' && auth[1]) {
-      try {
-        const data = toData(auth[1])
-        res.send({
-          message: 'Thanks for visiting the secret endpoint.',
-          data
-        })
-      }
-      catch(error) {
-        res.status(400).send({
-          message: `Error ${error.name}: ${error.message}`,
-        })
-      }
-    }
-    else {
-      res.status(401).send({
-        message: 'Please supply some valid credentials'
-      })
-    }
+router.get('/secret-endpoint', auth,(req, res) => {
+  res.send({
+    message: `Thanks for visiting the secret endpoint ${req.user.email}.`,
+  })
+  //   const auth = req.headers.authorization && req.headers.authorization.split(' ')
+  //   if (auth && auth[0] === 'Bearer' && auth[1]) {
+  //     try {
+  //       const data = toData(auth[1])
+  //       res.send({
+  //         message: 'Thanks for visiting the secret endpoint.',
+  //         data
+  //       })
+  //     }
+  //     catch(error) {
+  //       res.status(400).send({
+  //         message: `Error ${error.name}: ${error.message}`,
+  //       })
+  //     }
+  //   }
+  //   else {
+  //     res.status(401).send({
+  //       message: 'Please supply some valid credentials'
+  //     })
+  //   }
   })
 // define endpoints here
 module.exports = router
